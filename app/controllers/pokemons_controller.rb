c0bb4abce515e0d0a29b9002b762469f8a929a73
Samir Puranik <samir.puranik@berkeley.edu>
2018-03-21 20:26:24 -0700

    Can damage/destroy pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -5,4 +5,14 @@ class PokemonsController < ApplicationController
     to_capture.save
     redirect_to root_path
   end
+
+  def damage
+    to_damage = Pokemon.find(params[:id])
+    to_damage.health = to_damage.health - 10
+    to_damage.save
+    if to_damage.health <= 0
+      to_damage.destroy
+    end
+    redirect_to trainer_path(current_trainer)
+  end
 end
